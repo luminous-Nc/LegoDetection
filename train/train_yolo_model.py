731f1b514b4ae518f3167c5279c5f1a99681f6c4
cheng <chengniu@tamu.edu>
2023-07-25 02:20:59 -0500

    update train.py

--- train/train_yolo_model.py
+++ train/train_yolo_model.py
@@ -5,4 +5,4 @@ from ultralytics import YOLO
 if __name__ == "__main__":
     model = YOLO('../yolov8n.pt')  # load a pretrained model (recommended for training)
     # Train the model
-    model.train(data='lego_people.yaml', epochs=100, imgsz=640)
+    model.train(data='./train/lego_people.yaml', epochs=100, imgsz=640)
